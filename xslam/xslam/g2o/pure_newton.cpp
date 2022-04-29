4f5efdca9980191781d2451ae5967c3ec61eb0b0
duyongquan <duyongquan1@xiaomi.com>
2022-04-29 18:13:33 +0800

    add docs

--- xslam/xslam/g2o/pure_newton.cpp
+++ xslam/xslam/g2o/pure_newton.cpp
@@ -8,5 +8,34 @@ void PureNewtonSolver::RunDemo(bool visualization)
 
 }
 
+
+double PureNewtonSolver::ComputeFunctionValue(const Eigen::Vector2d& input)
+{
+
+  // x^2 + 3y^2 + 3xy + 4
+  double x = input(0);
+  double y = input(1);
+  return std::hypot(x, x) + 3 * std::hypot(y, y) + 3 * x * y + 4;
+}
+
+Eigen::Vector2d PureNewtonSolver::ComputeJacobianMatrix(const Eigen::Vector2d& input)
+{
+    // [2x + 3y]
+    // [6y + 3x]
+    return Eigen::Vector2d {
+        2 * input(0) + 3 * input(1),
+        3 * input(0) + 6 * input(1)
+    };
+}
+
+
+Eigen::Matrix2d PureNewtonSolver::ComputeHessianMatrix(const Eigen::Vector2d& iter_x)
+{
+    return Eigen::Matrix2d {
+
+    };
+}
+
+
 } // namespace g2o
 } // namespace xslam
--- xslam/xslam/g2o/pure_newton.h
+++ xslam/xslam/g2o/pure_newton.h
@@ -1,18 +1,41 @@
 #ifndef XSLAM_G2O_PURE_NEWTON_H
 #define XSLAM_G2O_PURE_NEWTON_H
 
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
 namespace xslam{
 namespace g2o {
 
 // https://github.com/TiantianUpup/numerical-optimization
 
+
+// Function 
+// f(x, y) =  x^2 + 3y^2 + 3xy + 4
 class PureNewtonSolver
 {
 public:
   void RunDemo(bool visualization = false);
+
+
+private:
+    // 计算f(x, y)的函数值
+    double ComputeFunctionValue(const Eigen::Vector2d& input);
+
+    // Jacobian matrix
+    Eigen::Vector2d ComputeJacobianMatrix(const Eigen::Vector2d& input);
+
+    // 计算hessian矩阵
+    // x: 迭代点
+    Eigen::Matrix2d ComputeHessianMatrix(const Eigen::Vector2d& iter_x);
+
+    Eigen::Vector2d state_;
 };
 
 } // namespace g2o
 } // namespace xslam
 
 #endif // XSLAM_G2O_PURE_NEWTON_H
+
+
+An Introduction to the Conjugate Gradient Method Without the Agonizing Pain
