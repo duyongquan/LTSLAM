52471be6d1c11dc29916db3d71b60b4172458e40
duyongquan <quandy2020@126.com>
2022-05-27 14:49:23 +0800

    tutorial

---
+++ tutorial/cpp/tutorial_cpp_003_simple_non_type_class_template_main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+#include "glog/logging.h"
+
+namespace tutorial {
+namespace cpp {
+namespace {
+
+} // namespace
+} // namespace cpp
+} // namespace tutorial
+
+int main(int argc, char **argv)
+{
+    LOG(INFO) << "Start tutorial cpp tutorial_cpp_003_simple_non_type_class_template_main ... ";;
+    return 0;
+}
