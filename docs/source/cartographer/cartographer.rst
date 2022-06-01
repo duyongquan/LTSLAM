268af104da5af6f9ac02ae0af8838e03e102dc68
duyongquan <quandy2020@126.com>
2022-06-01 20:13:35 +0800

    docs update

--- docs/source/cartographer/cartographer.rst
+++ docs/source/cartographer/cartographer.rst
@@ -4,7 +4,34 @@
 Cartographer
 ============
 
-.. toctree::
-    :maxdepth: 1
+1 Cartographer介绍
+=====================
 
+.. figure:: ./images/cartographer.png
+   :align: center
+
+2 Cartographer流程
+=====================
+
+.. figure:: ./images/high_level_system_overview.png
+   :align: center
+
+3 Scans
+=====================
+
+
+4 Submaps
+=====================
+
+
+5 Ceres scan matching
+=====================
+
+
+6 Optimization
+=====================
+
+
+7 Branch-and-bound scan matching
+================================
 
--- tutorial/cpp/tutorial_cpp_003_simple_non_type_class_template_main.cpp
+++ tutorial/cpp/tutorial_cpp_003_simple_non_type_class_template_main.cpp
@@ -7,12 +7,33 @@ namespace tutorial {
 namespace cpp {
 namespace {
 
+
+
+
+void RunCase1()
+{
+    
+    int ret = system(::std::string("lst -l").c_str());
+
+    if (ret == 0) {
+
+    }
+    LOG(INFO) << "ret = " << ret ;
+}
+
+void Run()
+{
+    RunCase1();
+}
+
+
 } // namespace
 } // namespace cpp
 } // namespace tutorial
 
 int main(int argc, char **argv)
 {
-    LOG(INFO) << "Start tutorial cpp tutorial_cpp_003_simple_non_type_class_template_main ... ";;
+    LOG(INFO) << "Start tutorial cpp tutorial_cpp_003_simple_non_type_class_template_main ... ";
+    tutorial::cpp::Run();
     return 0;
 }
