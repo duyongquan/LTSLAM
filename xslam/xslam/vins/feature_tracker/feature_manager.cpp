ce1eeeab1d6632fe641a19ee4deb60107957dbb1
duyongquan <quandy2020@126.com>
2022-06-02 14:51:36 +0800

    vins feature manager

---
+++ xslam/xslam/vins/feature_tracker/feature_manager.cpp
@@ -0,0 +1,17 @@
+#include "xslam/vins/feature_tracker/feature_manager.h"
+
+namespace xslam {
+namespace vins {
+namespace feature_tracker {
+
+FeatureManager::FeatureManager()
+{
+}
+
+FeatureManager::~FeatureManager()
+{
+}
+
+} // namespace feature_tracker
+} // namespace vins
+} // namespace xslam 
---
+++ xslam/xslam/vins/feature_tracker/feature_manager.h
@@ -0,0 +1,20 @@
+#ifndef XSLAM_VINS_FEATURE_MANAGER_H
+#define XSLAM_VINS_FEATURE_MANAGER_H
+
+namespace xslam {
+namespace vins {
+namespace feature_tracker {
+
+class FeatureManager
+{
+public:
+    FeatureManager();
+    ~FeatureManager();
+};
+
+
+} // namespace feature_tracker
+} // namespace vins
+} // namespace xslam 
+
+#endif // XSLAM_VINS_FEATURE_MANAGER_H
